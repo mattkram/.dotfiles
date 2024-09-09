a4dfbf2139c4b31829794cd2568da8bd239b5c79
mattkram <mkramer@anaconda.com>
2024-09-09 13:51:10 -0500

    Add line numbers

--- nvim/lua/mattkram/remap.lua
+++ nvim/lua/mattkram/remap.lua
@@ -1,6 +1,10 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- Add regular and relative line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
 -- Scroll up and down by page and center the view
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
