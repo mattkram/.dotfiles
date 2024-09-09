904ee86ae60bc0195cd4b1b6b839aed4a4dd8dd8
mattkram <mkramer@anaconda.com>
2024-09-09 11:44:54 -0500

    Add some global keymaps for paging up/down

--- nvim/lua/mattkram/remap.lua
+++ nvim/lua/mattkram/remap.lua
@@ -1,2 +1,6 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Scroll up and down by page and center the view
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
