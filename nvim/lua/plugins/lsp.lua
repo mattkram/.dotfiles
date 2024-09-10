7efbcd7bb080b6072c83f64d7a041d2cc8ea2485
mattkram <mkramer@anaconda.com>
2024-09-10 15:39:33 -0500

    Set new keybinding for formatting code

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -48,7 +48,7 @@ return {
                     vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
                     vim.keymap.set("n", "gs", vim.lsp.buf.signature_help, opts)
                     vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, opts)
-                    vim.keymap.set({ "n", "x" }, "<F3>", function() vim.lsp.buf.format({ async = true }) end, opts)
+                    vim.keymap.set({ "n", "x" }, "<leader>fc", function() vim.lsp.buf.format({ async = true }) end, opts)
                     vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
                 end,
             })
