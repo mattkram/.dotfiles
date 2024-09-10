eff2d2f03cb9a8152d1a2d9efea5c29fb3e4a547
mattkram <mkramer@anaconda.com>
2024-09-10 10:21:52 -0500

    Configure completions for LSP

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -19,17 +19,60 @@ return {
     },
     {
         "neovim/nvim-lspconfig",
+        dependencies = {
+            { 'hrsh7th/cmp-nvim-lsp' },
+            { 'hrsh7th/nvim-cmp' },
+        },
         config = function()
-            local lspconfig = require("lspconfig")
+            local lspconfig = require('lspconfig')
+
+            -- Add cmp_nvim_lsp capabilities settings to lspconfig
+            lspconfig.util.default_config.capabilities = vim.tbl_deep_extend(
+                'force',
+                lspconfig.util.default_config.capabilities,
+                require('cmp_nvim_lsp').default_capabilities()
+            )
+
+            -- Executes the callback function every time a
+            -- language server is attached to a buffer.
+            vim.api.nvim_create_autocmd('LspAttach', {
+                desc = 'LSP actions',
+                callback = function(event)
+                    local opts = { buffer = event.buf }
+
+                    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+                    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+                    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+                    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+                    vim.keymap.set("n", "go", vim.lsp.buf.type_definition, opts)
+                    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+                    vim.keymap.set("n", "gs", vim.lsp.buf.signature_help, opts)
+                    vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, opts)
+                    vim.keymap.set({ "n", "x" }, "<F3>", function() vim.lsp.buf.format({ async = true }) end, opts)
+                    vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+                end,
+            })
+
+            -- Add configuration for the individual language servers
             lspconfig.lua_ls.setup({})
-
             lspconfig.jedi_language_server.setup({})
-
             lspconfig.ruff.setup({})
+            lspconfig.rust_analyzer.setup({})
 
-            vim.keymap.set("n", "K", vim.lsp.buf.hover)
-            vim.keymap.set("n", "gd", vim.lsp.buf.definition)
-            vim.keymap.set({"n", "v"}, "<leader>ca", vim.lsp.buf.code_action)
+            -- Autocompletion setup
+            local cmp = require('cmp')
+
+            cmp.setup({
+                sources = {
+                    { name = 'nvim_lsp' },
+                },
+                snippet = {
+                    expand = function(args)
+                        vim.snippet.expand(args.body)
+                    end,
+                },
+                mapping = cmp.mapping.preset.insert({}),
+            })
         end
-    }
+    },
 }
