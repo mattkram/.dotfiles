1be19a5c22a4f69698fdcc3d85eaca2367da5727
mattkram <mkramer@anaconda.com>
2024-09-12 14:23:52 -0500

    Add a keybinding for test details and discover tests via pytest

--- nvim/lua/plugins/neotest.lua
+++ nvim/lua/plugins/neotest.lua
@@ -12,12 +12,14 @@ return {
             adapters = {
                 require("neotest-python")({
                     python = "./env/bin/python",
+                    pytest_discover_instances = true,
                 })
             }
         })
         vim.keymap.set('n', '<leader>tr', function() require("neotest").run.run() end )
         vim.keymap.set('n', '<leader>tf', function() require("neotest").run.run(vim.fn.expand("%")) end )
         vim.keymap.set('n', '<leader>ts', function() require("neotest").summary.toggle() end )
+        vim.keymap.set('n', '<leader>td', function() require("neotest").output_panel.toggle() end )
 
     end
 }
