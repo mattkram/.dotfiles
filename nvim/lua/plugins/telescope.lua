ad7f999c7f69ed24ec3837a6f9b020c542c1b851
mattkram <mkramer@anaconda.com>
2024-09-16 15:50:17 -0500

    Allow searching for hidden files in telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -4,6 +4,14 @@ return {
         tag = '0.1.8',
         dependencies = { 'nvim-lua/plenary.nvim' },
         config = function()
+            local telescope = require('telescope')
+            telescope.setup {
+                pickers = {
+                    find_files = {
+                        hidden = true
+                    }
+                }
+            }
             local builtin = require('telescope.builtin')
             vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
             vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
