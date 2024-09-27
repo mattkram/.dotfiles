0fafa6742f1ce5b0b6dab1cec42ff5d8b2d2ad81
mattkram <mkramer@anaconda.com>
2024-09-26 21:26:46 -0500

    Add nvim-treesitter-context

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,36 +1,41 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-        local configs = require("nvim-treesitter.configs")
-        vim.filetype.add({
-            filename = {
-                ['Tiltfile'] = 'starlark',
-            },
-        })
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            local configs = require("nvim-treesitter.configs")
+            vim.filetype.add({
+                filename = {
+                    ['Tiltfile'] = 'starlark',
+                },
+            })
 
-        configs.setup({
-            ensure_installed = {
-                "c",
-                "go",
-                "html",
-                "javascript",
-                "lua",
-                "python",
-                "query",
-                "starlark",
-                "vim",
-                "vimdoc",
-            },
-            auto_install = true,
-            sync_install = false,
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false,
-            },
-            indent = {
-                enable = true,
-            },
-        })
-    end
+            configs.setup({
+                ensure_installed = {
+                    "c",
+                    "go",
+                    "html",
+                    "javascript",
+                    "lua",
+                    "python",
+                    "query",
+                    "starlark",
+                    "vim",
+                    "vimdoc",
+                },
+                auto_install = true,
+                sync_install = false,
+                highlight = {
+                    enable = true,
+                    additional_vim_regex_highlighting = false,
+                },
+                indent = {
+                    enable = true,
+                },
+            })
+        end
+    },
+    {
+        "nvim-treesitter/nvim-treesitter-context",
+    }
 }
