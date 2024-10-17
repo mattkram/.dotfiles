0a7c068e74f43bb6125c5425bae4a5a0bbe4a450
mattkram <mkramer@anaconda.com>
2024-10-16 23:09:56 -0500

    Add keymap to run all tests

--- nvim/lua/plugins/neotest.lua
+++ nvim/lua/plugins/neotest.lua
@@ -22,6 +22,7 @@ return {
             },
         })
         vim.keymap.set('n', '<leader>tr', function() require("neotest").run.run() end)
+        vim.keymap.set('n', '<leader>ta', function() require("neotest").run.run(vim.fn.getcwd()) end)
         vim.keymap.set('n', '<leader>tf', function() require("neotest").run.run(vim.fn.expand("%")) end)
         vim.keymap.set('n', '<leader>ts', function() require("neotest").summary.toggle() end)
         vim.keymap.set('n', '<leader>td', function() require("neotest").output_panel.toggle() end)
