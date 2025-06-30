7289369bcf15edad149ee4550bb74adb43df1966
mattkram <mkramer@anaconda.com>
2025-06-30 11:03:25 -0500

    tidy: Split line

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -51,7 +51,8 @@ return {
                     vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, opts)
                     vim.keymap.set({ "n", "x" }, "<leader>fc", function() vim.lsp.buf.format({ async = true }) end, opts)
                     vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
-                    vim.keymap.set({ "n", "v" }, "<leader>fa", function() vim.lsp.buf.code_action({ async = true, apply = true }) end, opts)
+                    vim.keymap.set({ "n", "v" }, "<leader>fa",
+                        function() vim.lsp.buf.code_action({ async = true, apply = true }) end, opts)
                 end,
             })
 
