823507d8fec02c353b56faeb8294f2d8fddb0d1e
mattkram <mkramer@anaconda.com>
2025-08-25 09:34:37 -0500

    fix: Remove tab completion as it was breaking shell command completion

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -77,20 +77,7 @@ return {
                         vim.snippet.expand(args.body)
                     end,
                 },
-                mapping = cmp.mapping.preset.insert({
-                    ["<Tab>"] = cmp.mapping(function(fallback)
-                        -- This little snippet will confirm with tab, and if no entry is selected, will confirm the first item
-                        if cmp.visible() then
-                            local entry = cmp.get_selected_entry()
-                            if not entry then
-                                cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-                            end
-                            cmp.confirm()
-                        else
-                            fallback()
-                        end
-                    end, { "i", "s", "c", }),
-                }),
+                mapping = cmp.mapping.preset.insert({}),
             })
         end
     },
