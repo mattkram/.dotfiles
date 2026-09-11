4d9c8d6771887eee0a4fbac66345faa9eeab28c3
mattkram <mkramer@anaconda.com>
2026-09-11 16:52:46 -0500

    chore(nvim): remove redundant telescope branch pin

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,7 +1,6 @@
 return {
     {
         'nvim-telescope/telescope.nvim',
-        branch = 'master',
         dependencies = { 'nvim-lua/plenary.nvim' },
         config = function()
             local telescope = require('telescope')
